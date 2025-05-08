430b472228f59e4161c4f22d9fe109ad4bc0ef1f
lubaptista <luana.baptista@netglobecybersecurity.com.br>
2025-05-08 14:17:27 -0300

    cadastro, login e autenticação de usuário

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,20 @@
 import { Router } from 'express';
 
+import authMiddleware from './app/middlewares/auth';
+
 import UserController from './app/controllers/UserController';
+import SessionControler from './app/controllers/SessionControler';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
+
+routes.post('/sessions', SessionControler.store);
+
+// Todas as rotas abaixo deste middleware são acessíveis mediante autenticação
+routes.use(authMiddleware);
+
+routes.put('/users', UserController.update);
 
 
 export default routes;
